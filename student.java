8e27e721d80563709c22a3b2933ae1210be68a5b
Abubakar Masood <dormamu04@gmail.com>
2025-05-21 16:04:55 +0500

    Initial commit

---
+++ student.java
@@ -0,0 +1,24 @@
+public class Student {
+String name;
+int age;
+public Student(String name, int age) {
+    this.name=name;
+    this.age=age;
+}
+public void display() {
+    System.out.print("Name:" + name);
+    System.out.print("Age:" + age);
+}
+public static void main(String[] args) {
+    
+}
+}
+
+public class student {
+    public void display() {
+        String[] data= {"BMW","Suzuki","Toyota"};
+        //System.out.println(data[0]);
+        for(int=0;i<data;i++)
+        System.out.
+    }
+}
